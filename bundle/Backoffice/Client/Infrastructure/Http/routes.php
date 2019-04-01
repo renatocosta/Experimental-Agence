8319324feb723ab6234c970cb1884769999dd2d0
root <renatocostahome@gmail.com>
2019-04-01 21:44:16 +0000

    Set performance route

--- bundle/Backoffice/Client/Infrastructure/Http/routes.php
+++ bundle/Backoffice/Client/Infrastructure/Http/routes.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Backoffice\Client\Infrastructure\Http\Controllers'], function() {
 
   Route::get('client', ['uses' => 'ClientController@getAll']);      
-  /*Route::post('Client/performance', ['uses' => 'ClientByPerformanceController@listByCriteria']);  
-  Route::post('Client/performanceandaverage', ['uses' => 'ClientByPerformanceController@listByCriteriaAndAverage']);  */
+  Route::post('client/performance', ['uses' => 'ClientByPerformanceController@listByCriteria']);  
+  /*Route::post('Client/performanceandaverage', ['uses' => 'ClientByPerformanceController@listByCriteriaAndAverage']);  */
  
 });    
