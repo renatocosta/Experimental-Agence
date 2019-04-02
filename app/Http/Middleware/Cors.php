1ddf6bd71a29513d4bae38bc3e428774a5e17d04
root <renatocostahome@gmail.com>
2019-04-02 02:47:32 +0000

    Remove cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,16 +8,16 @@ class Cors {
 
     public function handle($request, Closure $next) {
 
-        if (in_array(env('APP_ENV'), ['local', 'dev'])) {
+    //    if (in_array(env('APP_ENV'), ['local', 'dev'])) {
             
             return $next($request)
                             ->header('Access-Control-Allow-Origin', '*')
                             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, X-Auth-Token, content-type');
-        }   
+  //      }   
         
         
-        return $next($request);
+//        return $next($request);
         
     }
 
